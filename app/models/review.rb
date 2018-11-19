ac5d134fb2bb1e817fe2a1f4c3ba96efdf6dca52
dustincrs <dustincrs@outlook.com>
2018-11-19 16:38:20 +0800

    Fixed validation for Review rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,7 +4,7 @@ class Review < ApplicationRecord
 	belongs_to :project
 
 	# Validations
-	validates :rating, presence: true, numericality: { minimum: 0, maximum: 5 }
+	validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 	validates :user_id, :project_id, presence: true
 
 	# CONSTANTS
