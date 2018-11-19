85e3d6186b5984bbe342893e2f4dbff472b41151
dustincrs <dustincrs@outlook.com>
2018-11-19 18:30:33 +0800

    Add status to Project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_19_074510) do
+ActiveRecord::Schema.define(version: 2018_11_19_100335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2018_11_19_074510) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.boolean "is_complete", default: true
+    t.integer "status", default: 0
   end
 
   create_table "responses", force: :cascade do |t|
