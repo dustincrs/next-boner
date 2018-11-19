148fc9af5ca2ff651e098803d8cac4c827f9b962
dustincrs <dustincrs@outlook.com>
2018-11-19 16:07:11 +0800

    Add code to display rating as FA Icons

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,24 @@ class Review < ApplicationRecord
 	# Associations
 	belongs_to :user
 	belongs_to :project
+
+	# Validations
+	validates :rating, presence: true, numericality: { minimum: 0, maximum: 5 }
+	validates :user_id, :project_id, presence: true
+
+	# CONSTANTS
+	SYMBOLS = {
+				star_empty: "<i class=\"far fa-star\"></i>",
+				star_full: "<i class=\"fas fa-star\"></i>",
+				}
+
+
+	def star_rating
+		result = [SYMBOLS[:star_empty]] * 5
+		rating.times do |i|
+			result[i] = SYMBOLS[:star_full]
+		end
+
+		return result.join("").html_safe
+	end
 end
