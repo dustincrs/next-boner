a2629a30a769f6b4a4ec9d114ef6d41c2937dee6
dustincrs <dustincrs@outlook.com>
2018-11-21 15:37:55 +0800

    Add basic functionality for approving and hiding responses

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -11,6 +11,8 @@ class ProjectsController < ApplicationController
   # GET /projects/1
   # GET /projects/1.json
   def show
+    @pending = @project.responses.where(is_approved: false, is_hidden: false)
+    @approved = @project.responses.where(is_approved: true, is_hidden: false)
   end
 
   # GET /projects/new
--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,6 +1,7 @@
 class ResponsesController < ApplicationController
 	before_action :require_login
 	before_action :get_associated_project, only: [:show]
+	before_action :get_response, only: [:update]
 
 	def show
 	end
@@ -15,12 +16,30 @@ class ResponsesController < ApplicationController
 		end
 
 		redirect_to project_path(response_params[:project_id])
-		
+	end
+
+	def update
+		# Make is_hidden to true
+		if(params[:_method]=="patch")
+			@response.is_hidden = true
+			@response.is_approved = false
+			@response.save!
+
+		elsif(params[:_method]=="put")
+			@response.is_approved = true
+			@response.save!
+		end
+
+		redirect_to project_path(@response.project)
 	end
 
 	private
 	def get_associated_project
 		@project = Project.find_by_id(params[:id])
+	end
+
+	def get_response
+		@response = Response.find_by_id(params[:id])
 	end
 
 	def response_params
