d0fdf8dcfbea24d136cdf558666ead8d8ed4d069
dustincrs <dustincrs@outlook.com>
2018-11-23 12:23:37 +0800

    Tidied up Project#new.

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,12 @@ class Project < ApplicationRecord
 	# CONSTANTS
 	CATEGORIES = [["Select a Category", ""], "Heavy Lifting", "Food", "Automotive", "Sharing", "Advice"]
 
+	SYMBOLS = { 
+	 			clock: '<i class="far fa-clock"></i>'.html_safe,
+	 			person: '<i class="fas fa-people-carry"></i>'.html_safe,
+	 			map: '<i class="fas fa-map-marked-alt"></i>'.html_safe,
+	 			}
+
 	# Enumerations
 	enum status: [:empty, :has_vacancies, :almost_full, :full]
 
