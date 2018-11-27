e9bea90d74e898b10c3a4e5bb6db887f039da9a0
dustincrs <dustincrs@outlook.com>
2018-11-27 16:56:05 +0800

    Added chatrooms to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,17 @@ def generate_user(role)
 	return u
 end
 
+def generate_test_user
+	u = User.new()
+	u.first_name = Faker::Name.first_name
+	u.last_name = Faker::Name.last_name
+	u.date_of_birth = Faker::Date.birthday(18, 75)
+	u.phone_number = "1234567890"
+	u.email = "test@email.com"
+	u.password = "1234567890"
+	return u
+end
+
 def generate_project(owner_id)
 	n_p = Project.new()
 	n_p.title = Faker::Hipster.sentence
@@ -67,6 +78,14 @@ end
 
 report_seeding(seeded_users, users_to_seed, "users")
 
+# Seed a test account
+if generate_test_user.save
+	puts "Seeded test@email.com, password: 1234567890"
+else
+	puts "Failed to seed test user!"
+end
+
+
 # Seed projects
 total_projects = 0
 saved_projects = 0
@@ -76,6 +95,7 @@ User.all.each do |user|
 		new_project = generate_project(user.id)
 		if(new_project.save)
 			saved_projects += 1
+			new_chatroom = Chatroom.create(project_id: new_project.id)
 		end
 	end
 end
