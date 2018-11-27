8706d9d191848e11a1b706c2f2fa733125cdb9dd
Abdalaziz <abdalaziz.ahmed5@gmail.com>
2018-11-28 05:51:02 +0800

    fix autocomplete bug

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -29,3 +29,34 @@
 
 })
 
+
+       $(document).ready(function(){
+        $("#keyupz").on('keyup', function(e){
+            console.log("hello");
+
+            $.ajax({
+                url:"/projects",
+                method:"GET",
+                data: $("#keyupz").serialize(),
+                dataType:"json",
+                success: function(data){
+
+                    console.log(data);
+                    var box = document.getElementById("titles")
+                    box.innerHTML = ""
+                    data.forEach(function(project){
+                        var item = document.createElement("option")
+                        item.value = project.title
+                        box.append(item)
+                    })
+
+                    // body...
+                }
+        })
+
+    })
+
+
+
+})
+
